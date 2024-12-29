d539180b90ab1dcdeca14cfffd0219c29549ca47
keita <developer@milkcocoa.info>
2024-12-30 01:32:42 +0900

    :bug: TL取得処理がtransaction内になかったので修正

--- server/application/service/src/main/kotlin/com/milkcocoa/info/shochu_club/server/service/TimelineServiceImpl.kt
+++ server/application/service/src/main/kotlin/com/milkcocoa/info/shochu_club/server/service/TimelineServiceImpl.kt
@@ -5,6 +5,7 @@ import com.milkcocoa.info.shochu_club.server.domain.model.feed.FeedSummaryDataOb
 import com.milkcocoa.info.shochu_club.server.domain.repository.DistributionBackend
 import com.milkcocoa.info.shochu_club.server.domain.repository.FeedRepository
 import com.milkcocoa.info.shochu_club.server.domain.service.TimelineService
+import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 import kotlin.uuid.ExperimentalUuidApi
 import kotlin.uuid.Uuid
 
@@ -18,78 +19,76 @@ class TimelineServiceImpl(
         cursor: String?,
         count: Int
     ): List<FeedSummaryDataObject> {
-        return feedRepository.homeTimeline(
-            uid = uid,
-            cursor = cursor,
-            count = count
-        ).map { feed ->
-            when(feed){
-                is FeedSummaryDataObject.NormalFeedData -> {
-                    feed.copy(
-                        owner = feed.owner.copy(
-                            iconUrl = when(feed.owner.iconUrl){
-                                is StoredMediaObject.Image.UnResolved ->{
-                                    StoredMediaObject.Image.Resolved(
-                                        url = distributionBackend.generateSignedUrl(
-                                            bucket = "",
-                                            key = "",
-                                            expires = 2000L
-                                        ),
-                                        resolution = (feed.owner.iconUrl as StoredMediaObject.Image.UnResolved).resolution
-                                    )
+        return newSuspendedTransaction {
+            return@newSuspendedTransaction feedRepository.homeTimeline(
+                uid = uid,
+                cursor = cursor,
+                count = count
+            ).map { feed ->
+                when(feed){
+                    is FeedSummaryDataObject.NormalFeedData -> {
+                        feed.copy(
+                            owner = feed.owner.copy(
+                                iconUrl = when(feed.owner.iconUrl){
+                                    is StoredMediaObject.Image.UnResolved ->{
+                                        StoredMediaObject.Image.Resolved(
+                                            url = distributionBackend.generateSignedUrl(
+                                                key = (feed.owner.iconUrl as StoredMediaObject.Image.UnResolved).key,
+                                                expires = 2000L
+                                            ),
+                                            resolution = (feed.owner.iconUrl as StoredMediaObject.Image.UnResolved).resolution
+                                        )
+                                    }
+                                    else -> feed.owner.iconUrl
                                 }
-                                else -> feed.owner.iconUrl
-                            }
-                        ),
-                        storedMediaObjects = feed.storedMediaObjects.map { media ->
-                            when(media){
-                                is StoredMediaObject.Image.UnResolved ->{
-                                    StoredMediaObject.Image.Resolved(
-                                        url = distributionBackend.generateSignedUrl(
-                                            bucket = "",
-                                            key = "",
-                                            expires = 2000L
-                                        ),
-                                        resolution = media.resolution
-                                    )
-                                }
-                                is StoredMediaObject.Image.Resolved ->{
-                                    // 現実的にはResolvedの状態で入ってくることはないはず。
-                                    // Repositoryレイヤでは画像の解決はできないはずなので。
-                                    media
-                                }
-                                is StoredMediaObject.Image.NoData ->{
-                                    media
-                                }
-                                is StoredMediaObject.Video.UnResolved ->{
-                                    StoredMediaObject.Video.Resolved(
-                                        url = distributionBackend.generateSignedUrl(
-                                            bucket = "",
-                                            key = "",
-                                            expires = 2000L
-                                        ),
-                                        thumbnails = media.thumbnails.map { thumbs ->
-                                            StoredMediaObject.Image.Resolved(
-                                                url = distributionBackend.generateSignedUrl(
-                                                    bucket = "",
-                                                    key = "",
-                                                    expires = 2000L
-                                                ),
-                                                resolution = thumbs.resolution
-                                            )
-                                        }
-                                    )
-                                }
-                                is StoredMediaObject.Video.Resolved ->{
-                                    // 画像と同じく、現実的にはResolvedの状態で入ってくることはないはず。
-                                    // Repositoryレイヤで動画の解決はできないはずなので。
-                                    media
+                            ),
+                            storedMediaObjects = feed.storedMediaObjects.map { media ->
+                                when(media){
+                                    is StoredMediaObject.Image.UnResolved ->{
+                                        StoredMediaObject.Image.Resolved(
+                                            url = distributionBackend.generateSignedUrl(
+                                                key = media.key,
+                                                expires = 2000L
+                                            ),
+                                            resolution = media.resolution
+                                        )
+                                    }
+                                    is StoredMediaObject.Image.Resolved ->{
+                                        // 現実的にはResolvedの状態で入ってくることはないはず。
+                                        // Repositoryレイヤでは画像の解決はできないはずなので。
+                                        media
+                                    }
+                                    is StoredMediaObject.Image.NoData ->{
+                                        media
+                                    }
+                                    is StoredMediaObject.Video.UnResolved ->{
+                                        StoredMediaObject.Video.Resolved(
+                                            url = distributionBackend.generateSignedUrl(
+                                                key = "",
+                                                expires = 2000L
+                                            ),
+                                            thumbnails = media.thumbnails.map { thumbs ->
+                                                StoredMediaObject.Image.Resolved(
+                                                    url = distributionBackend.generateSignedUrl(
+                                                        key = "",
+                                                        expires = 2000L
+                                                    ),
+                                                    resolution = thumbs.resolution
+                                                )
+                                            }
+                                        )
+                                    }
+                                    is StoredMediaObject.Video.Resolved ->{
+                                        // 画像と同じく、現実的にはResolvedの状態で入ってくることはないはず。
+                                        // Repositoryレイヤで動画の解決はできないはずなので。
+                                        media
+                                    }
                                 }
                             }
-                        }
-                    )
+                        )
+                    }
                 }
-            }
-        }.toList()
+            }.toList()
+        }
     }
 }
